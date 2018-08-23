f3e29a129f5f4c9ff23f0f57cfc94c11bb2f69c7
Mark John Merin <v-mamer@microsoft.com>
2018-08-23 12:36:58 -0700

    make script more generic

--- macos_metrics.rb
+++ macos_metrics.rb
@@ -2,7 +2,8 @@ require 'rest-client'
 require 'json'
 require 'date'
 
-url = 'https://supermarket.chef.io/api/v1/cookbooks/macos'
+cookbook = ARGV[0]
+url = "https://supermarket.chef.io/api/v1/cookbooks/#{cookbook}"
 response = RestClient.get(url)
 parsed = JSON.parse(response)
 highest_download = ['0.0.0', 0]
@@ -23,7 +24,8 @@ current_time = DateTime.now
 formatted_time = current_time.strftime '%m/%d/%Y %H:%M'
 
 printf "\n"
-printf "#{formatted_time} Metrics: \n"
-printf "  macos-cookbook total downloads: #{total} \n"
-printf "  Most popular macos-cookbook is %s: %s \n",
+printf "#{cookbook} cookbook metrics: \n"
+printf "  Date/Time: #{formatted_time} \n"
+printf "  Total Downloads: #{total} \n"
+printf "  Most Popular Version: %s at %s downloads \n",
        highest_download[0], highest_download[1]
