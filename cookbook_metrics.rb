94f46c9d020f32fd519dd2a075b2c5f5adad26e1
Mark John Merin <v-mamer@microsoft.com>
2018-08-23 12:58:20 -0700

    add better formatting and labels

--- cookbook_metrics.rb
+++ cookbook_metrics.rb
@@ -2,6 +2,11 @@ require 'rest-client'
 require 'json'
 require 'date'
 
+if ARGV.empty?
+  printf "Please specify a cookbook name to see metrics. \n"
+  exit
+end
+
 cookbook = ARGV[0]
 url = "https://supermarket.chef.io/api/v1/cookbooks/#{cookbook}"
 response = RestClient.get(url)
@@ -10,8 +15,16 @@ highest_download = ['0.0.0', 0]
 
 total = parsed['metrics']['downloads']['total']
 version_array = parsed['metrics']['downloads']['versions']
+source_url = parsed["source_url"]
+desc = parsed["description"]
 sorted = version_array.sort_by { |a, _b| Gem::Version.new(a) }
 
+printf "\n=========  #{cookbook} cookbook metrics  =========\n"
+printf "Description: #{desc}\n"
+printf "Source URL:  #{source_url}\n\n"
+
+printf "Version Downloads\n"
+printf "------- ---------\n"
 sorted.each do |version|
   if version[1] > highest_download[1]
     highest_download = version
@@ -21,11 +34,12 @@ sorted.each do |version|
 end
 
 current_time = DateTime.now
-formatted_time = current_time.strftime '%m/%d/%Y %H:%M'
+formatted_time = current_time.strftime '%m/%d/%Y - %H:%M'
 
 printf "\n"
-printf "#{cookbook} cookbook metrics: \n"
+printf "Metrics: \n"
 printf "  Date/Time: #{formatted_time} \n"
 printf "  Total Downloads: #{total} \n"
-printf "  Most Popular Version: %s at %s downloads \n",
+printf "  Most Downloaded Version: %s at %s downloads \n\n",
        highest_download[0], highest_download[1]
+printf "=================================================\n"
